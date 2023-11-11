f36788bf17a52d40f3fee1c9c10b631e48c75af4
Perky0509 <s.boswallperks@gmail.com>
2023-11-11 23:22:56 +0000

    A class that is able to tell you details about df, including it's shape and datatypes

--- DataFrameInfo.py
+++ DataFrameInfo.py
@@ -3,9 +3,9 @@ import pandas as pd
 import csv 
 import numpy as np
 
+## FULLY COPIED TO PY VERSION ##
 
-loan_payments = pd.read_csv('loan_payments.csv')
-trial_df = loan_payments.copy()
+loans_for_info = pd.read_csv('tansformed_loan_payments.csv')
 
 """This class allows the user to find out more about the dataframe, enabling greater understanding of what needs to be altered
 and can be done in terms of manipulation and calculations."""
@@ -38,9 +38,11 @@ class DataFrameInfo():
         percentage = self.df.isna().sum() / len(self.df) * 100
         return percentage.round()
     
-    
 
-loan = DataFrameInfo(loan_payments)
+loan = DataFrameInfo(loans_for_info)
 
 categ_list = ['sub_grade', 'grade', 'loan_status']
+print(loan.unique_categories(categ_list))
 
+#saving updated file for next class 
+loans_for_info.to_csv('loans_for_info.csv')
