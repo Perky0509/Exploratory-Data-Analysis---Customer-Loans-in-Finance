ed35de731ca93a145c563e0e09b7477890c1e129
Perky0509 <s.boswallperks@gmail.com>
2023-11-18 23:37:21 +0000

    Dealing with missing vals and skew to be able to look into what the data means, as well as visualising the transformations

--- Plotter_DataFrameTransform.py
+++ Plotter_DataFrameTransform.py
@@ -10,28 +10,30 @@ import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor 
 
 
-df_transform = pd.read_csv('loans_for_info.csv')
-
+# ------------------------------------------------------------------------- Transforming Data Frame -------------------------------------------------------------------------- # 
+#opening csv file
+transformed_loan_payments = pd.read_csv('transformed_loan_payments.csv')
 
 """first we must establish the percentage of missing/null values within each column of the df. Once we've done that we can either delete rows, delete columns or impute. 
 This will depend on how high the percentage is (if huge delete col, if not big, but significant stil remove rows, and if tiny impute using mean)"""
 
 #calculating the percentage of null values in the df, shown per column
-percent_null_values = round(df_transform.isna().sum() / len(df_transform) * 100, 1) 
+percent_null_values = round(transformed_loan_payments.isna().sum() / len(transformed_loan_payments) * 100, 1) 
 #print(percent_null_values)
+
 
 #Because the columns with high, but under 65% nulls were key columns (eg next_payment_date - 60.1%) without other cols with similar purpose, the % required to be in this list is 65%
 cols = ['mths_since_last_delinq', 'mths_since_last_record']
 
 #remove columns that have a high % of null/missing vals. 
-def remove_cols(df_transform, cols : list):
+def remove_cols(transformed_loan_payments, cols : list):
     for col in cols:
-        df_transform = df_transform.drop(col, axis= 1)    
-    return df_transform.info()
-remove_cols(df_transform, cols)
+        transformed_loan_payments = transformed_loan_payments.drop(col, axis= 1)    
+    return transformed_loan_payments.info()
+remove_cols(transformed_loan_payments, cols)
 
 #calculating the percent of non-null values per row
-percent_null_in_rows = df_transform.apply(lambda x: round(x.count() / len(df_transform.axes[1]) * 100), axis=1)
+percent_null_in_rows = transformed_loan_payments.apply(lambda x: round(x.count() / len(transformed_loan_payments.axes[1]) * 100), axis=1)
 
 #calculating how many rows have below 80% non-null values. 
 high_null_rows = percent_null_in_rows < 80
@@ -39,9 +41,10 @@ print(high_null_rows.value_counts())
 #output = 0 so no rows to be deleted 
 
 #saving numeric columns as separate df
-numeric_cols = df_transform.select_dtypes(include='number')
+numeric_cols = transformed_loan_payments.select_dtypes(include='number')
 numeric_cols
 numeric_cols.to_csv('numeric_cols.csv')
+
 
 """Class to perform manipulations of the table in order to allow for clean and effective analysis. No rows are to be cut so the methods will be to remove columns and impute"""
 
@@ -53,7 +56,7 @@ class DataFrameTransform():
     #for normally distributed data
     def impute_mean_data(self, cols: list):
         for col in cols:
-            self.df[col].fillna(self.df[col].mean())
+           self.df[col] = self.df[col].fillna(self.df[col].mean())
         return self.df[col].info()
 
     #for category data
@@ -68,7 +71,7 @@ class DataFrameTransform():
             self.df[col] = self.df[col].fillna(self.df[col].median())
         return self.df[col].info()    
 
-     #log transformation 
+    #log transformation 
     def log_transformation_and_vis(self, col):
         self.col = self.df[col]
         np.random.seed(0)
@@ -101,44 +104,30 @@ class DataFrameTransform():
         IQR = Q3 - Q1
         self.col = self.df[(self.col >= Q1 - 1.5*IQR) & (self.col <= Q3 + 1.5*IQR)]
         sns.boxplot(self.col)
-        plt.title('no_outliers')
+        plt.title(f'{col} - no_outliers')
         plt.show()
     
-loan_df = DataFrameTransform(df_transform)
+loan_df = DataFrameTransform(transformed_loan_payments)
 
 #median chosen because of the nature of the distributions
 median_cols = ['int_rate', 'last_payment_amount', 'collections_12_mths_ex_med', 'collections_12_mths_ex_med', 'mths_since_last_major_derog']
 print(loan_df.impute_median_data(median_cols))
-print(loan_df.box_cox_transformation('annual_inc'))
-print(loan_df.log_transformation_and_vis('annual_inc'))
-print(loan_df.remove_outliers(['total_rec_late_fee']))
 
 
 #using the numeric df created above to pass through a correlation matrix
 data_for_matrix = pd.read_csv('numeric_cols.csv').dropna()
 corr_matrix = data_for_matrix.corr().abs()
 corr_matrix
+
 #the level of high correlation is set to anything above 0.9
 high_corr = corr_matrix > 0.9
-print(high_corr)
+high_corr #number of high correlations: 38
+
 #using the results of the it is determined that the below columns should be dropped. 
-cols_to_drop = ['id', 'funded_amount', 'instalment', 'total_rec_int']
-transformed_loan_payments = df_transform.drop(cols_to_drop, axis=1)
-transformed_loan_payments.head()   
+cols_to_drop = ['id', 'total_rec_int', 'policy_code', 'application_type']
+transformed_loan_payments = transformed_loan_payments.drop(cols_to_drop, axis=1)
 
-loan_df = DataFrameTransform(df_transform)
-
-#eg to test
-mean_cols = ['int_rate', 'funded_amount']
-print(loan_df.impute_mean_data(mean_cols))
-
-
-df_transform.to_csv('df_transform.csv')
-
-
-"""now we move onto creating a class that illustrates the data tranformation with regards to imputing missing vals"""
-
-plotting = pd.read_csv('df_transform.csv')
+# ------------------------------------------------------------------- Plotting Data Transformations ------------------------------------------------------------------- #
 
 
 class Plotter():
@@ -149,23 +138,15 @@ class Plotter():
     def qq_plot(self, col : str): 
         self.col = self.df[col]
         self.qq_plott = sm.qqplot(self.col, scale= 1, line= 'q', fit=True)
-        plt.title('self.col')
+        plt.title(f'{col}')
         plt.show()
-
+    
     def boxplot(self, col: str):
         self.col = self.df[col]
         self.boxplot = sns.boxplot(self.col)
         plt.show()
 
+loan_df = Plotter(transformed_loan_payments)
 
-histogram = ['int_rate']
-
-loan_df = Plotter(plotting)
-
-#testing code
-print(loan_df.qq_plot(['total_rec_late_fee']))
-print(loan_df.boxplot('total_rec_late_fee'))
-
-
-#saving a new csv copy
-plotting.to_csv('plotting.csv')
+#saving file
+transformed_loan_payments.to_csv('transformed_loan_payments.csv')
