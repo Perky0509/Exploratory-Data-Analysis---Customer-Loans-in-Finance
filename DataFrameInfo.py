2b057e291c85c265600f5c265be93cd56af412d3
Perky0509 <s.boswallperks@gmail.com>
2023-11-18 23:34:59 +0000

    Enables you to see some key features of the df before using it for analysis

--- DataFrameInfo.py
+++ DataFrameInfo.py
@@ -1,11 +1,9 @@
-
+# ------------------------------------------------------------------------------- DataFrame Info ---------------------------------------------------------------------------------- #
 import pandas as pd 
 import csv 
 import numpy as np
 
-## FULLY COPIED TO PY VERSION ##
-
-loans_for_info = pd.read_csv('tansformed_loan_payments.csv')
+transformed_loan_payments = pd.read_csv("transformed_loan_payments.csv")
 
 """This class allows the user to find out more about the dataframe, enabling greater understanding of what needs to be altered
 and can be done in terms of manipulation and calculations."""
@@ -39,10 +37,6 @@ class DataFrameInfo():
         return percentage.round()
     
 
-loan = DataFrameInfo(loans_for_info)
+loan = DataFrameInfo(transformed_loan_payments)
 
-categ_list = ['sub_grade', 'grade', 'loan_status']
-print(loan.unique_categories(categ_list))
-
-#saving updated file for next class 
-loans_for_info.to_csv('loans_for_info.csv')
+transformed_loan_payments.to_csv('transformed_loan_payments')
